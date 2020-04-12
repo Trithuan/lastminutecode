f01891e1c44314196f75f6672e51f8a8c9e23376
Rysek <36196503+rezatera854@users.noreply.github.com>
2020-04-13 00:02:41 +0200

    ajout du cv

--- lastminutecode/views.py
+++ lastminutecode/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 
 def mathieu(request):
-	return render(request, 'mathieu.html')
+	return render(request, 'mathieu/index.html')
 
 def cv_mathieu(request):
 	return render(request, 'mathieu/cv.html')
